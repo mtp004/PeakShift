92b3e44bcf5a7fddda25712c3dd92d19c11a2a7e
Tri Pham <tri.pham121@icloud.com>
2025-07-20 16:29:59 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DailyRateChart from './components/DailyRateChart';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PeakShift - Electric Usage Optimizer</h1>
+      <DailyRateChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DailyRateChart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DailyRateChart() {
+  return <div>📊 Daily Rate Chart will go here</div>;
+}
+
+export default DailyRateChart;
